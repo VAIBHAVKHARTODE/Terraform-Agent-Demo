14a34db73a4d2ed3fda0fe85a368917ad0fd6e9b
PUSH TO PROD <vaibhavkhartode001@gmail.com>
2026-08-05 16:31:09 +0530

    Refactor S3 bucket versioning to use configuration block
    Updated S3 bucket versioning configuration to use a block.

--- main.tf
+++ main.tf
@@ -14,7 +14,9 @@ resource "aws_s3_bucket" "example" {
 
 resource "aws_s3_bucket_versioning" "example" {
   bucket = aws_s3_bucket.example.bucket
-  versioning_enabled = true
+
+  versioning_configuration {
+  status = "Enabled"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
